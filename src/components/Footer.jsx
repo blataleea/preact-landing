5ed1132a9a1e739a5ff81785f9ba048419f9f95f
Ana Lee <orange665321@gmail.com>
2024-11-15 01:24:44 +0300

    concept and draft

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import {h} from 'preact';
+import styles from '../styles/Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__logo}>
+          <span className={styles.footer__company_name}>ООО «ХАРД ФМ»</span>
+        </div>
+        <div className={styles.footer__copyrighting}>
+          ООО ХАРД ФМ. ВСЕ ПРАВА ЗАЩИЩЕНЫ
+        </div>
+        <div className={styles.footer__contacts}>
+          <p>Email: info@company.com</p>
+          <p>Tel: +7 (925)-275-2555</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 4000,
+    host: '0.0.0.0',
+  },
+  plugins: [preact()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use 'src/styles/_variables.scss' as *;`
+      }
+    }
+  }
+});
